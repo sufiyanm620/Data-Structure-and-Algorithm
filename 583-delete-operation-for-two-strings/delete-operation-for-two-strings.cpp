8b6f6447c6817e4f1994d00bd1b3555506368ed4
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-07-15 18:54:35 +0530

    Time: 13 ms (42.50%) | Memory: 16.9 MB (21.40%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -3,10 +3,7 @@ public:
     int minDistance(string s1, string s2) {
         int m=s1.size();
         int n=s2.size();
-        if(s1=="a"&&s2=="b") return 2;
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        for(int i=0;i<m;i++) dp[i][0]=0;
-        for(int i=0;i<n;i++) dp[0][i]=0;
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(s1[i-1]==s2[j-1]){
