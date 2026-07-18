f612b4d47a3cce74b70286c8308e41913653ae71
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-07-18 16:07:00 +0530

    Time: 1440 ms (15.60%) | Memory: 71.9 MB (99.18%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -6,7 +6,7 @@ public:
             int start = bookings[i][0];
             int end = bookings[i][1];
             int m = bookings[i][2];
-            for(int j=start-1;j<=end-1;j++){
+            for(int j=start-1;j<end;j++){
                 res[j]+=m;
             }
         }
