a3e5066b25d94236a87e52f1101a391bf2f2a359
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-07-24 22:57:19 +0530

    Time: 7 ms (62.09%) | Memory: 35.1 MB (73.28%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -3,34 +3,34 @@ public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int m=maze.size();
         int n=maze[0].size();
-        vector<vector<int>> grid(m,vector<int>(n,0));
+        vector<vector<int>> directions{{1,0},{0,1},{-1,0},{0,-1}};
+        queue<pair<int,int>> que;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(maze[i][j]=='+')
-                grid[i][j]=1;
-            }
-        }
-        vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
-        queue<vector<int>> que;
-        int steps=0;
-        que.push({entrance[0],entrance[1],0});
-        while(!que.empty()){
-            auto curr=que.front();
-            int x=curr[0];
-            int y=curr[1];
-            int steps=curr[2];
-            que.pop();
-            maze[entrance[0]][entrance[1]]=1;
-            if((x!=entrance[0]||y!=entrance[1])&&(x==0||y==0||x==m-1||y==n-1))
-               return steps;
-            for(auto &dir:directions){
-                int ni=x+dir[0];
-                int nj=y+dir[1];
-                if(ni>=0&&nj>=0&&ni<m&&nj<n&&grid[ni][nj]==0){
-                    que.push({ni,nj,steps+1});
-                    grid[ni][nj]=1;
+                if((i==0||j==0||i==m-1||j==n-1)&&(maze[i][j]=='.')&&(i!=entrance[0]||j!=entrance[1])){
+                    que.push({i,j});
+                    maze[i][j]='+';
                 }
             }
+        }
+        if(que.empty()) return -1;
+        int steps=0;
+        while(!que.empty()){
+            int k=que.size();
+            while(k--){
+            auto [x,y]=que.front();
+            que.pop();
+            if(x==entrance[0]&&y==entrance[1]) return steps;
+            for(auto &dir:directions){
+                int ni=x+dir[0];
+                int nj=y+dir[1]; 
+                if(ni>=0&&nj>=0&&ni<m&&nj<n&&maze[ni][nj]=='.'){
+                    maze[ni][nj]='+';
+                    que.push({ni,nj});
+                }
+            }
+        }
+        steps++;
         }
         return -1;
     }
