fb9d373f5f09f25ee89e87a6a86cd150ed1e226f
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-08-02 11:54:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (92.92%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    int n;
-    void dfs(vector<vector<int>>& isConnected,int u,vector<bool> &vis){
-        vis[u]=true;
-        for(int i=0;i<n;i++){
-            if(isConnected[u][i]&&!vis[i]){
-                dfs(isConnected,i,vis);
-            }
+    vector<int> rank;
+    vector<int> parent;
+    int find(int x){
+        if(x==parent[x]) return x;
+        return parent[x]=find(parent[x]);
+    }
+    void Union(int x,int y){
+        int xp=find(x);
+        int yp=find(y);
+        if(xp==yp) return;
+        if(rank[xp]>rank[yp]) parent[yp]=xp;
+        else if(rank[yp]>rank[xp]) parent[xp]=yp;
+        else{
+            parent[xp]=yp;
+            rank[yp]++;
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-         n = isConnected.size();
-         int provinces=0;
-        vector<bool> vis(n,false);
+        int n=isConnected.size();
+        rank.resize(n,0);
+        parent.resize(n);
+        for(int i=0;i<n;i++) parent[i]=i;
+        int count=n;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(isConnected,i,vis);
-                provinces++;
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]&&find(i)!=find(j)){
+                    Union(i,j);
+                    count--;
+                }
             }
         }
-        return provinces;
+        return count;
 
     }
 };
