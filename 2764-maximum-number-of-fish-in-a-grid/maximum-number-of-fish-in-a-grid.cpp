eacd47dd3284958d22178f60353b188c12e6e335
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-08-10 20:22:13 +0530

    Time: 14 ms (37.37%) | Memory: 95.9 MB (70.86%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
+    int total=0;
+    vector<vector<int>> dir{{1,0},{0,1},{-1,0},{0,-1}};
+    void dfs(int r,int c,vector<vector<int>>& grid,int m,int n,int &total){
+        total+=grid[r][c];
+        grid[r][c]=0;
+        for(auto &d:dir){
+        int ni=r+d[0];
+        int nj=c+d[1];
+        if(ni>=0&&nj>=0&&ni<m&&nj<n&&grid[ni][nj]!=0){
+            dfs(ni,nj,grid,m,n,total);
+        }
+        } 
+    }
     int findMaxFish(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         int ans=0;
-        vector<vector<int>> dir{{1,0},{0,1},{-1,0},{0,-1}};
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int total=0;
-                queue<pair<int,int>> que;
                 if(grid[i][j]!=0){
-                    total+=grid[i][j];
-                    que.push({i,j});
-                    grid[i][j]=0;
-                   
-                    while(!que.empty()){
-                      auto [x,y]=que.front();
-                       que.pop();
-                      for(auto &d:dir){
-                        int ni=x+d[0];
-                        int nj=y+d[1];
-                        if(ni<m&&nj<n&&ni>=0&&nj>=0&&grid[ni][nj]!=0){
-                            total+=grid[ni][nj];
-                            que.push({ni,nj});
-                            grid[ni][nj]=0;
-                        }
-                      }
-                    }
+                dfs(i,j,grid,m,n,total);
                 }
                 ans=max(ans,total);
             }
@@ -34,3 +30,23 @@ public:
         return ans;
     }
 };
+// int total=0;
+//                 queue<pair<int,int>> que;
+//                 if(grid[i][j]!=0){
+//                     total+=grid[i][j];
+//                     que.push({i,j});
+//                     grid[i][j]=0;
+//                     while(!que.empty()){
+//                       auto [x,y]=que.front();
+//                        que.pop();
+//                       for(auto &d:dir){
+//                         int ni=x+d[0];
+//                         int nj=y+d[1];
+//                         if(ni<m&&nj<n&&ni>=0&&nj>=0&&grid[ni][nj]!=0){
+//                             total+=grid[ni][nj];
+//                             que.push({ni,nj});
+//                             grid[ni][nj]=0;
+//                         }
+//                       }
+//                     }
+//                 }
