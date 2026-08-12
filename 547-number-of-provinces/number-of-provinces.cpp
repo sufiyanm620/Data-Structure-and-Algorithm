a54fd7dd1f0fa48a653d1d53253690b5c85603d7
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-08-12 12:32:20 +0530

    Time: 3 ms (36.20%) | Memory: 19.2 MB (97.70%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
-    vector<int> rank;
-    vector<int> parent;
-    int find(int x){
-        if(x==parent[x]) return x;
-        return parent[x]=find(parent[x]);
-    }
-    void Union(int x,int y){
-        int xp=find(x);
-        int yp=find(y);
-        if(xp==yp) return;
-        if(rank[xp]>rank[yp]) parent[yp]=xp;
-        else if(rank[yp]>rank[xp]) parent[xp]=yp;
-        else{
-            parent[xp]=yp;
-            rank[yp]++;
+    void dfs(vector<vector<int>>& graph,int u,vector<bool>& vis){
+        vis[u]=true;
+        for(int v=0;v<graph.size();v++){
+        if(!vis[v]&&graph[u][v]){
+            dfs(graph,v,vis);
+        }
         }
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        rank.resize(n,0);
-        parent.resize(n);
-        for(int i=0;i<n;i++) parent[i]=i;
-        int count=n;
+    int findCircleNum(vector<vector<int>>& graph) {
+        int n = graph.size();
+        int count=0;
+        vector<bool> vis(n,false);
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(isConnected[i][j]&&find(i)!=find(j)){
-                    Union(i,j);
-                    count--;
-                }
+            if(!vis[i]){
+                dfs(graph,i,vis);
+                count++;
             }
         }
         return count;
-
     }
 };
