89124545bc9bc75874f491bacb13fbd3e7475c55
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-08-14 20:33:13 +0530

    Time: 0 ms (100.00%) | Memory: 119.7 MB (81.48%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int n;
-    long long solve(vector<vector<int>>& nums,int i,vector<long long> &dp){
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        long long pick=nums[i][0]+solve(nums,i+1+nums[i][1],dp);
-        long long skip=solve(nums,i+1,dp);
-        return dp[i]=max(pick,skip);
-    }
     long long mostPoints(vector<vector<int>>& nums) {
-        n=nums.size();
+        int n=nums.size();
         vector<long long> dp(n+1,-1);
-        return solve(nums,0,dp);
+        for(int i=n-1;i>=0;i--){
+            long long pick =nums[i][0];
+            int j=i+nums[i][1]+1;
+            if(j<n)
+              pick+=dp[j];
+          long long skip=dp[i+1];
+          dp[i]=max(pick,skip);
+        }
+        return dp[0];
     }
 };
