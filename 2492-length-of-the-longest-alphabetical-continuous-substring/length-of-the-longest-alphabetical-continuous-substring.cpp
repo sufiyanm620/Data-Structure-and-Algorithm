c7aa2c844347e50e4761ee5060da596f98b3e847
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-08-23 17:42:53 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (58.74%) - LeetSync

--- 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     int longestContinuousSubstring(string s) {
        int n=s.size();
-       int ans=0;
+       int ans=1;
        int count=1;
        for(int i=1;i<n;i++){
-            if(s[i]-s[i-1]==1){
-                count++;
-            }else{
-                count=1;
-            }
+            if(s[i]-s[i-1]==1) count++;
+            else count=1;
             ans=max(ans,count);
        }
-       return max(ans,count);
+       return ans;
     }
 };
