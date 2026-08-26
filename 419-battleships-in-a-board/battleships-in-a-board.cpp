3f8af4d4e47c892ab4e6a32fd07cf7071a4a1f54
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-08-26 13:19:50 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (84.03%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/battleships-in-a-board.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
+    vector<vector<int>> dir{{-1,0},{1,0},{0,-1},{0,1}};
+    int m;
+    int n;
+    void dfs(int i,int j,vector<vector<char>> &board){
+        board[i][j]='.';
+        for(auto &d:dir){
+            int ni=i+d[0];
+            int nj=j+d[1];
+            if(ni<0||nj<0||ni>=m||nj>=n) continue;
+            if(board[ni][nj]=='X')
+              dfs(ni,nj,board);
+        }
+    }
     int countBattleships(vector<vector<char>>& board) {
-        int m=board.size();
-        int n=board[0].size();
-        vector<vector<int>> dir{{-1,0},{0,-1},{0,1},{1,0}};
-        queue<pair<int,int>> que;
+         m=board.size();
+         n=board[0].size();
         int count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]=='X'){
                     count++;
-                    que.push({i,j});
-                    while(!que.empty()){
-                        auto [x,y]=que.front();
-                        que.pop();
-                        for(auto &d:dir){
-                            int ni=x+d[0];
-                            int nj=y+d[1];
-                            if(ni>=0&&nj>=0&&ni<m&&nj<n&&board[ni][nj]=='X'){
-                                que.push({ni,nj});
-                                board[ni][nj]='.';
-                            }
-                        }
-                    }
+                    dfs(i,j,board);
                 }
             }
         }
