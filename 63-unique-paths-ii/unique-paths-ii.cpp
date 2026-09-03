aca127d0411d1842ca499a9383ba2a3abc85221f
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-09-03 23:46:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (55.47%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     int m;
     int n;
-    int solve(vector<vector<int>>& grid,int i,int j,vector<vector<int>> &dp){
-        if(i>=m||j>=n) return 0;
+    vector<vector<int>> dp;
+    int solve(int i,int j,vector<vector<int>> &grid){
         if(i==m-1&&j==n-1) return 1;
+        if(i>=m||j>=n) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int right = 0;
-        if(i+1<m&&grid[i+1][j]!=1) 
-            right = solve(grid,i+1,j,dp);
-        int down = 0;
+        int right=0;
+        if(i+1<m&&grid[i+1][j]!=1)
+          right=solve(i+1,j,grid);
+        int down=0;
         if(j+1<n&&grid[i][j+1]!=1)
-            down = solve(grid,i,j+1,dp);
+            down=solve(i,j+1,grid);
         return dp[i][j]=right+down;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         m=grid.size();
         n=grid[0].size();
-        if(grid[0][0]==1) return 0;
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(grid,0,0,dp);
+        if(grid[0][0]==1||grid[m-1][n-1]==1) return 0;
+        dp.assign(m,vector<int>(n,-1));
+        return solve(0,0,grid);
     }
 };
