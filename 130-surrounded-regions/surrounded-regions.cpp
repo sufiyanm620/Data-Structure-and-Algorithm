8824252971d2682211471210212f0b015dac455a
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-09-04 18:50:19 +0530

    Time: 3 ms (21.19%) | Memory: 16.3 MB (10.41%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
-    typedef pair<int,int> p;
     void solve(vector<vector<char>>& board) {
         int m=board.size();
         int n=board[0].size();
-        queue<p> que;
+        vector<vector<int>> dir{{0,1},{0,-1},{-1,0},{1,0}};
+        vector<vector<int>> vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
-            if(board[i][0]=='O'){
-              board[i][0]='#';
-              que.push({i,0});
-            }
-            if(board[i][n-1]=='O'){
-                board[i][n-1]='#';
-                que.push({i,n-1});
-            }
-        }
-        for(int j=1;j<n-1;j++){
-            if(board[0][j]=='O'){
-                board[0][j]='#';
-                que.push({0,j});
-            }
-            if(board[m-1][j]=='O'){
-                board[m-1][j]='#';
-                que.push({m-1,j});
-            }
-        }
-        while(!que.empty()){
-            p curr=que.front();
-            int x = curr.first;
-            int y = curr.second;
-            que.pop();
-            for(auto &dir:directions){
-                int ni=x+dir[0];
-                int nj=y+dir[1];
-                if(ni>=0&&nj>=0&&ni<m&&nj<n&&board[ni][nj]=='O'){
-                          board[ni][nj]='#';
-                          que.push({ni,nj});
+            for(int j=0;j<n;j++){
+                if((board[i][j]=='O')&&(i==0||j==0||i==m-1||j==n-1)){
+                    queue<pair<int,int>> que;
+                    vis[i][j]=1;
+                    que.push({i,j});
+                    while(!que.empty()){
+                        int sz=que.size();
+                        while(sz--){
+                            auto [r,c]=que.front();
+                            que.pop();
+                            for(auto &d:dir){
+                                int nr=r+d[0];
+                                int nc=c+d[1];
+                                if(nr>=0&&nc>=0&&nr<m&&nc<n&&board[nr][nc]=='O'&&!vis[nr][nc]){
+                                    vis[nr][nc]=1;
+                                    que.push({nr,nc});
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]!='#')
-                   board[i][j]='X';
-                else 
-                    board[i][j]='O';
+                if(!vis[i][j])
+                  board[i][j]='X';
             }
         }
+        
     }
 };
