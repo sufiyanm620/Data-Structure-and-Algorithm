8823ead8bd9d8e5f39f8989862c7c3e6c16e4bf3
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-09-05 22:41:07 +0530

    Time: 171 ms (13.61%) | Memory: 46.5 MB (12.49%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int m=heights.size();
-        int n=heights[0].size();
-        vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        vector<vector<int>> directions{{1,0},{0,1},{-1,0},{0,-1}};
-        pq.push({0,0,0});
+        vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
+        vector<vector<int>> dir{{-1,0},{0,-1},{0,1},{1,0}};
         dist[0][0]=0;
+        int wt=0;
+        pq.push({0,0,0});
         while(!pq.empty()){
             auto curr=pq.top();
-            int wt=curr[0];
+            pq.pop();
+            wt=curr[0];
             int x=curr[1];
             int y=curr[2];
-            pq.pop();
             if(x==m-1&&y==n-1) return wt;
             if(wt>dist[x][y]) continue;
-            for(auto &dir:directions){
-                int ni=x+dir[0];
-                int nj=y+dir[1];
-                if(ni>=0&&nj>=0&&ni<m&&nj<n){
-                    int nwt=max(dist[x][y],abs(heights[x][y]-heights[ni][nj]));
+            for(auto &d:dir){
+                int ni=x+d[0];
+                int nj=y+d[1];
+                if(ni<m&&nj<n&&ni>=0&&nj>=0){
+                    int nwt=max(abs(grid[x][y]-grid[ni][nj]),dist[x][y]);
                     if(nwt<dist[ni][nj]){
                         dist[ni][nj]=nwt;
                         pq.push({nwt,ni,nj});
