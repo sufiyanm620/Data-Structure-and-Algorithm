e26f680dc599e457abdba3ff7a2c49d3e79a3bd8
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-09-07 21:57:53 +0530

    Time: 8 ms (33.47%) | Memory: 10.1 MB (32.96%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -8,15 +8,18 @@ public:
         int count=0;
         if(k>n) return false;
         for(int i=0;i<k;i++){
-              freq[s2[i]]++;
+              if(mp.find(s2[i])!=mp.end()&&freq[s2[i]]<mp[s2[i]]) count++;
+                freq[s2[i]]++;
         }
-        if(mp==freq) return true;
+        if(count==k) return true;
         for(int i=k;i<n;i++){
-             freq[s2[i]]++;
+        
+            if(mp.find(s2[i])!=mp.end()&&freq[s2[i]]<mp[s2[i]]) count++;
+                freq[s2[i]]++;
              freq[s2[i-k]]--;
-             if(freq[s2[i-k]]==0)
-               freq.erase(s2[i-k]);
-            if(freq==mp) return true;
+            if(mp.find(s2[i-k])!=mp.end()&&freq[s2[i-k]]<mp[s2[i-k]]) count--;
+           
+            if(count==k) return true;
         }
         return false;
     }
