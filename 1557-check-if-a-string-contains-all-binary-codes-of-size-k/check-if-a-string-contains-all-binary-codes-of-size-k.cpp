5274409dc71e64d4303157e620d3c5a8b10b21d9
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-09-08 18:48:22 +0530

    Time: 1093 ms (5.06%) | Memory: 132.7 MB (24.51%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        map<string,int> mp;
-        int n=s.size();
-        int count=0;
-        for(int i=0;i<=n-k;i++){
-            string res="";
-            for(int j=i;j<i+k;j++){
-                res+=s[j];
-            }
-            if(mp.find(res)==mp.end()) count++;
-            mp[res]++;
-            
-        }
-        return count==(1<<k);
+         int n=s.size();
+         map<string,int> mp;
+         string res="";
+         if(n<k) return false;
+         for(int i=0;i<k;i++){
+            res+=s[i];
+         }
+         mp[res]++;
+         for(int i=k;i<n;i++){
+                res.erase(0,1);
+                res+=s[i];
+                mp[res]++;
+         }
+         return mp.size()==1<<k;
     }
 };
-    
+    // map<string,int> mp;
+    //     int n=s.size();
+    //     int count=0;
+    //     for(int i=0;i<n-k;i++){
+    //         string res="";
+    //         for(int j=i;j<i+k;j++){
+    //             res+=s[j];
+    //         }
+    //         if(mp.find(res)==mp.end()) count++;
+    //         mp[res]++;
+            
+    //     }
+    //     return count==pow(2,k);
