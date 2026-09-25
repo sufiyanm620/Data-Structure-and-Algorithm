595a49985f78a1d73ac83d989b111ba10767afb4
Mohd_Sufiyan_620 <sufiyanmohd548@gmail.com>
2026-09-25 19:54:01 +0530

    Time: 39 ms (94.56%) | Memory: 29 MB (92.35%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,23 +6,20 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<n-2;i++){
             if(i>0&&nums[i]==nums[i-1]) continue;
-             int l=i+1;
-             int r=n-1;
-             while(l<r){
-                int total=nums[i]+nums[l]+nums[r];
-                if(total==0){
-                    ans.push_back({nums[i],nums[l],nums[r]});
-                    while(l<r&&nums[l]==nums[l+1]) l++;
-                    while(l<r&&nums[r]==nums[r-1]) r--;
-                    l++;
-                    r--;
-                }
-                else if(total<0){
-                    l++;
-                }else{
-                    r--;
-                }
-             }
+            int l=i+1;
+            int r=n-1;
+            while(l<r){
+                  int sum=nums[i]+nums[l]+nums[r];
+                  if(sum==0){
+                  ans.push_back({nums[i],nums[l],nums[r]});
+                  l++;
+                  r--;
+                while(l<r&&nums[l]==nums[l-1]) l++;
+                while(l<r&&nums[r]==nums[r+1]) r--;
+                  }
+                else if(sum>0) r--;
+                else l++;
+            }
         }
         return ans;
     }
